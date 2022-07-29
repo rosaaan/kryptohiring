7875a70ff1cbdc37d1f5e9ad62b6e463d8ed9dff
rosaaan <89770635+rosaaan@users.noreply.github.com>
2022-07-29 19:30:36 +0530

    Add files via upload

---
+++ kryp.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+function App() {
+  return (
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ShopKart</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      </ul>
+</div>
+  </div>
+</nav>
+  );
+}
+
+export default App;
